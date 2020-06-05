54e6d78206085b31b8a34714168fb83396b1c19e
Aakash <aakash1350@gmail.com>
2020-06-06 00:53:46 +0530

    Name

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,19 +10,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   List currencies;
+  final List<MaterialColor> _colors = [Colors.blue, Colors.indigo, Colors.red];
 
   @override
-  void initState(){
+  void initState() async {
     super.initState();
     currencies = await getCurrencies();
   }
 
   Future<List> getCurrencies() async {
-    String cryptoUrl = "";
-  } 
-  
+    String cryptoUrl = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
+    http.Response response = await http.get(cryptoUrl);
+    return json.decode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,9 +39,27 @@ class _HomePageState extends State<HomePage> {
     return Container(
       child: Flexible(
         child: ListView.builder(
-          itemCount: 0,
-          itemBuilder: (BuildContext context, int index) {},
+          itemCount: currencies.length,
+          itemBuilder: (BuildContext context, int index) {
+            final Map currency = currencies[index];
+            final MaterialColor color = _colors[index % _colors.length];
+
+            return _getListItem(currency, color);
+          },
         ),
+      ),
+    );
+  }
+
+  ListTile _getListItem(Map currency, MaterialColor color) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: color,
+        child: Text(currency['name'][0]),
+      ),
+      title: Text(
+        currency['name'],
+        style: TextStyle(fontWeight: FontWeight.bold),
       ),
     );
   }
